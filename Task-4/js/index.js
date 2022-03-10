ff85f37a50c8cdeb755b8fbacff4f04d0dbe520c
Daniel Rhoades <daniel.rhoades.94@gmail.com>
2022-03-10 14:24:04 -0600

    Include alert for forgetting to name task

--- Task-4/js/index.js
+++ Task-4/js/index.js
@@ -1,21 +1,28 @@
 function validFormFieldInput(event) {
   event.preventDefault();
+  alertElement = document.getElementById('bootstrap-alert'
+  );
+  alertElement.hidden = true;
 
   const taskElement = document.getElementById('task');
   const taskValue = taskElement.value;
   console.log("Task Name: "+taskValue);
-
+  if(taskValue === '') {
+    alertElement.hidden = false;
+    alertElement.innerHTML = "Don't forget to name your task!";
+  }
+  
   const assigneeElement = document.getElementById('assignee');
   const assigneeValue = assigneeElement.value;
-  console.log("Assigned To: "+assigneeValue);
+  //console.log("Assigned To: "+assigneeValue);
 
   const dueElement = document.getElementById('due');
   const dueValue = dueElement.value;
-  console.log("Due Date: "+dueValue);
+  //console.log("Due Date: "+dueValue);
 
   const descriptionElement = document.getElementById('description');
   const descriptionValue = descriptionElement.value;
-  console.log("Task Description: "+descriptionValue);
+  //console.log("Task Description: "+descriptionValue);
 }
 
 submitElement = document.getElementById('submit-form');
