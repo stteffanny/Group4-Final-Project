41227de6f02392fa5692c42a0dcf6525ef34c6ae
Derrick James <95151579+flx1derrick@users.noreply.github.com>
2022-03-28 10:28:26 -0500

    task-card styles

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -8,7 +8,10 @@ createTaskHtml = (name, description, assignee, dueDate, status, id) => {
     <div class="task card">
         <div class="card-header">
             <div class="float-left">
-                Due : ${dueDate}
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
+  <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
+  <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"/>
+</svg> ${dueDate}
             </div>
             <div class="float-right">
                 Status: <span class="badge badge-primary">${status}</span>
@@ -21,8 +24,10 @@ createTaskHtml = (name, description, assignee, dueDate, status, id) => {
         <p class="card-text">${description}</p>
         <a href="#" class="badge badge-warning">
             ${assignee}</a>
-        <button type="button" class="btn btn-success done-button float-right">Mark As Done</button>
-        <button type="button" class="btn btn-danger delete-button float-right">Delete</button>
+        <div class = "btn-group float-right">
+        <button type="button" class="small btn btn-outline-success done-button ">Mark As Done</button>
+        <button type="button" class="btn btn-outline-danger delete-button float-right">Delete</button>
+        </div>
     </div>
 </li>`;
     return html;
@@ -63,7 +68,7 @@ class TaskManager {
         for (let i = 0; i < this.tasks.length; i++) {
             let currentTask = this.tasks[i];
             let date = new Date(currentTask.due);
-            let formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+            let formattedDate = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
             let taskHtml = createTaskHtml(currentTask.name, currentTask.description, currentTask.assignee, formattedDate, currentTask.status, currentTask.id);
             tasksHtmlList.push(taskHtml);
         };
